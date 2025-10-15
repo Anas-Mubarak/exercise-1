13911ce02bf620896284f532b306383f57f93734
Anas-Mubarak <anasmubarak435@gmail.com>
2025-10-15 17:43:24 +0530

    tas 6 complete

--- fetchusers/fetchusers.js
+++ fetchusers/fetchusers.js
@@ -1,4 +1,6 @@
 let userboxes = document.querySelectorAll('.userbox')
+let seachname = document.querySelector('.searchname')
+let userfilter = document.querySelector('.userfilter')
 
 async function fetchusers() {
     let response = await fetch('https://randomuser.me/api/?results=5')
@@ -15,12 +17,35 @@ function main({results})
     for(let userbox of userboxes)
     {
         let {picture:{large},name:{title,first,last},email} = results[i]
-        // console.log(title+first+last)
-        // console.log(large)
-        console.log()
         userbox.children[0].style.background = `url(${large}) no-repeat`
         userbox.children[1].children[0].innerText = title+ ' '+ first+' '+last
         userbox.children[1].children[1].innerText = email
         i++
     }
+
+    userfilter.addEventListener('click',()=>{
+        let fullnamelist = []
+        for(let i=0;i<5;i++)
+        {
+            let {name:{title,first,last}} = results[i]
+            fullnamelist.push(title+' '+first+' '+last)
+        }
+        console.log(seachname.value)
+        let regexpression = seachname.value==''?new RegExp('.*'):new RegExp(seachname.value)
+        let matchlist = fullnamelist.map((username)=>regexpression.test(`${username}`))
+        console.log(matchlist)
+        let i = 0
+        for(let userbox of userboxes)
+        {
+            if(matchlist[i])
+            {
+                console.log(userbox)
+                userbox.style.display = ''
+            }
+            else{
+                userbox.style.display = 'none'
+            }
+            i++
+        }
+    })
 }
